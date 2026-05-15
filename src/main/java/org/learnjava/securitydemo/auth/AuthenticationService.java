7a48b5d2c5e056ee18a23a9e962386c22c725591
Anas Hatem <74430931+anashatem46@users.noreply.github.com>
2026-05-15 19:16:25 +0300

    add authentication and registration endpoints with JWT support

---
+++ src/main/java/org/learnjava/securitydemo/auth/AuthenticationService.java
@@ -0,0 +1,55 @@
+package org.learnjava.securitydemo.auth;
+
+
+import lombok.AllArgsConstructor;
+import org.learnjava.securitydemo.config.JwtService;
+import org.learnjava.securitydemo.user.Role;
+import org.learnjava.securitydemo.user.User;
+import org.learnjava.securitydemo.user.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new
+                        UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+}
+
+
--- src/main/java/org/learnjava/securitydemo/config/JwtService.java
+++ src/main/java/org/learnjava/securitydemo/config/JwtService.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Service
-class JwtService {
+public class JwtService {
 
     private final String secretKey;
 
